b8731965e2b39baee4675bd9651c87e479683d82
Kostadin Raychev <k.raychev@abv.bg>
2026-04-01 17:20:20 +0300

    changing the path to a global one

--- read_external_file.py
+++ read_external_file.py
@@ -1,6 +1,6 @@
 from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
 
-fn = "/eos/cms/store/user/mileva/bsm3g/NANO/ZprimeTo2Tau-2Jets_M-1000_TuneCP5_13p6TeV_madgraphMLM-pythia8/Run3Summer23_NANOAODv12/250603_104516/0000/nanoaodsim_1.root"
+fn = "root://cms-xrd-global.cern.ch//eos/cms/store/user/mileva/bsm3g/NANO/ZprimeTo2Tau-2Jets_M-1000_TuneCP5_13p6TeV_madgraphMLM-pythia8/Run3Summer23_NANOAODv12/250603_104516/0000/nanoaodsim_1.root"
 
 print(f"File path: {fn}\n")
 print("="*80 + "\n")
